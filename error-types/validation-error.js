a77d48deff002bde911a5da7b2fe74c86ff9d26d
cjbelt <cjbeltran@protonmail.com>
2023-08-16 12:17:54 -0300

    error classes created

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,39 @@
 // Your code here
+class ValidationError extends Error {
+  constructor(message = 'Invalid input', ...params) {
+    super(...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.name = 'ValidationError';
+    this.message = message;
+  }
+}
+
+// class Field {
+//   constructor(inputName, value) {
+//     this.inputName = inputName;
+//     this.value = value;
+//   }
+// }
+
+// // When a message is passed in:
+// let passwordInput = new Field('Password', 'pass');
+// // Field { inputName: 'Password', value: 'pass' }
+// if (passwordInput.value === 'pass') {
+//   throw new ValidationError('Insecure password');
+// }
+// // throws ValidationError: Insecure password
+
+// // When a message is not passed in:
+// passwordInput = new Field('Password', 10);
+// // Field { inputName: 'Password', value: 10 }
+// if (typeof passwordInput.value !== 'string') {
+//   throw new ValidationError();
+// }
+// // throws ValidationError: Invalid input
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
